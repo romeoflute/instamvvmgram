550b4cd7a5a44bc918d3ebef0e8a40377dc42eb1
meoflauta <meoflauta@gmail.com>
2020-11-09 20:24:39 +0800

    Update tab icons and remove tab titles

--- InstaMVVMgram/ContentView.swift
+++ InstaMVVMgram/ContentView.swift
@@ -8,34 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab){
             FeedView()
                 .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Feed")
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
                 }
+                .tag(0)
             SearchView()
                 .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
+                    Image(systemName: selectedTab == 1 ? "magnifyingglass.circle.fill" : "magnifyingglass.circle")
                 }
+                .tag(1)
             AddPostView()
                 .tabItem {
-                    Image(systemName: "plus.circle.fill")
-                    Text("Add Post")
+                    Image(systemName: selectedTab == 2 ? "plus.circle.fill" : "plus.circle" )
                 }
+                .tag(2)
             NotificationsView()
                 .tabItem {
-                    Image(systemName: "rectangle.fill.badge.checkmark")
-                    Text("Notifications")
+                    Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
                 }
+                .tag(3)
             ProfileView()
                 .tabItem {
-                    Image(systemName: "person.circle.fill")
-                    Text("Profile")
+                    Image(systemName: selectedTab == 4 ? "person.fill" : "person")
                 }
+                .tag(4)
         }
+        .onAppear() {
+            UITabBar.appearance().isTranslucent = true
+            UITabBar.appearance().backgroundColor = .white
+        }
+        .accentColor(.black)
     }
 }
 
